c5500e22249f1523e24212c6b153d2c846eff345
Allen Soberano <allensoberano@gmail.com>
2018-04-27 08:26:37 -0700

    App Crashes on mAlsoKnownTV.setText((CharSequence) sandwich.getAlsoKnownAs());

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -43,7 +44,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +57,17 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+
+        TextView mAlsoKnownTV = (TextView) findViewById(R.id.also_known_tv);
+        TextView mIngredientsTV = (TextView) findViewById(R.id.ingredients_tv);
+        TextView mOriginTV = (TextView) findViewById(R.id.origin_tv);
+        TextView mDescriptionTV = (TextView) findViewById(R.id.description_tv);
+
+        mAlsoKnownTV.setText((CharSequence) sandwich.getAlsoKnownAs());
+        mOriginTV.setText(sandwich.getPlaceOfOrigin());
+
+
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,66 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwich = null;
+
+        try {
+
+            JSONObject sandwichDetails = new JSONObject(json);
+            JSONObject sandwhichName = sandwichDetails.getJSONObject("name");
+
+            String mainName = sandwhichName.getString("mainName");
+            JSONArray alsoKnownAs = sandwhichName.getJSONArray("alsoKnownAs");
+
+            String placeOfOrigin = sandwichDetails.getString("placeOfOrigin");
+            String description = sandwichDetails.getString("description");
+            String imageLink = sandwichDetails.getString("image");
+            JSONArray ingredients = sandwichDetails.getJSONArray("ingredients");
+
+            //Start sandwich making
+            sandwich = new Sandwich();
+
+            //using setters from Sandwich object to create the sandwhich information
+            sandwich.setMainName(mainName);
+            sandwich.setPlaceOfOrigin(placeOfOrigin);
+            sandwich.setDescription(description);
+            sandwich.setImage(imageLink);
+
+            //Converts to List<string> before setting
+            sandwich.setAlsoKnownAs(convertJSONArray(alsoKnownAs));
+            sandwich.setIngredients(convertJSONArray(ingredients));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return sandwich;
     }
+
+    //Converts JSONArray to List<String>
+    private static List<String> convertJSONArray(JSONArray jArr) throws JSONException {
+        List<String> arrList = null;
+
+        if (jArr.length() > 0){
+            arrList = new ArrayList<>();
+            for(int i=0; i<jArr.length(); i++) {
+                arrList.add(jArr.getString(i));
+            }
+
+            return arrList;
+        }
+
+        return arrList;
+    }
+
 }
+
